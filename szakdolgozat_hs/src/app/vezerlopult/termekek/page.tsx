ae6d762ae8d7d8d03a0e3f54ae11ef7ea7ccd081
Leccgoo <adamszenti@gmail.com>
2025-03-29 11:58:19 +0100

    Add product UI

--- szakdolgozat_hs/src/app/vezerlopult/termekek/page.tsx
+++ szakdolgozat_hs/src/app/vezerlopult/termekek/page.tsx
@@ -4,13 +4,15 @@ import { Product } from "@/app/termekek/page"
 import { Button, ButtonGroup } from "react-bootstrap"
 import { useEffect, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons"
+import { faPen, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { IconProp } from "@fortawesome/fontawesome-svg-core"
 import { enqueueSnackbar } from "notistack"
+import fallbackImg from '../../../../public/static/imgNotFound.png'
 
 export default function Page() {
 
     const [products, setProducts] = useState<Product[]>([])
+    //const [newProduct, setNewProducts] = useState<Product>()
 
     const categoryOptions: { [key: number]: string } = {
         1: "Videókártya",
@@ -63,7 +65,14 @@ export default function Page() {
 
 
     return (
-        <div className="container-fluid position-relative px-0">
+        <div className="container-fluid position-relative px-0 productContainer">
+            <div className="d-flex justify-content-between">
+                <h1 className="mb-3 mb-sm-0">Termékek</h1>
+                <button className="orangeButton" data-bs-toggle="modal" data-bs-target="#productModal">
+                    Termék hozzáadása
+                </button>
+            </div>
+            <hr />
             <div className="row dashboardRowHeader">
                 <div className="col-1">
                     ID
@@ -128,6 +137,206 @@ export default function Page() {
                     </div>
                 </div>
             ))}
+
+            <div className="modal fade" id="productModal" tabIndex={-1} aria-labelledby="productModal" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-fullscreen">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Termék hozzáadása</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            
+                            <div className="row justify-content-center justify-content-xxl-start">
+                                <div className="col-12 col-sm-10 col-md-8 col-lg-12 col-xxl-6 order-2 order-xxl-1 mt-5 mt-xxl-0">
+                                    {/*<ProductSwiper images={product.imageUrls} />*/}
+                                    <div className="mb-3">
+                                        <label className="form-label">Képek kezelése:</label>
+                                        <div className="image-list mb-3">
+                                            <div /*key={index}*/ className="d-flex align-items-center mb-2">
+                                                    <img
+                                                        src={/*image.url || */fallbackImg.src}
+                                                        style={{ width: '50px', height: '50px', objectFit: 'contain', marginRight: '10px' }}
+                                                    />
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        /*value={image.url != fallbackImg.src ? image.url : ""}
+                                                        onChange={(e) => handleChange(e, undefined, index)}*/
+                                                        placeholder="Kép URL"
+                                                    />
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-danger btn-sm ms-2"
+                                                        /*onClick={() => {
+                                                            const newImageUrls = product.imageUrls.filter((_, i) => i !== index)
+                                                            setProduct(prevState => prevState ? ({ ...prevState, imageUrls: newImageUrls }) : null)
+                                                            setDeletedImages(prevState => [...prevState, image.url])
+                                                        }}*/
+                                                    >
+                                                        <FontAwesomeIcon icon={faTrash as IconProp} />
+                                                    </button>
+                                                </div>
+                                            <button
+                                                className="addImgButton"
+                                                /*onClick={() => {
+                                                    setProduct(prevState => {
+                                                        if (!prevState) return null
+                                                        return {
+                                                            ...prevState,
+                                                            imageUrls: [...prevState.imageUrls, { url: fallbackImg.src }]
+                                                        }
+                                                    })
+                                                }}*/
+                                            >
+                                                Új kép hozzáadása
+                                                <span><FontAwesomeIcon icon={faPlus as IconProp} /></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-xxl-6 order-1 order-xxl-2">
+                                    <form /*onSubmit={handleSubmit}*/>
+                                        <div className="mb-3">
+                                            <label htmlFor="name" className="form-label">Név:</label>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                name="name"
+                                                className="form-control"
+                                                /*value={product.name}
+                                                onChange={handleChange}*/
+                                            />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="price" className="form-label">Ár:</label>
+                                            <input
+                                                type="number"
+                                                id="price"
+                                                name="price"
+                                                className="form-control"
+                                                /*value={product.price}
+                                                onChange={handleChange}*/
+                                            />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="manufacturer" className="form-label">Gyártó:</label>
+                                            <input
+                                                type="text"
+                                                id="manufacturer"
+                                                name="manufacturer"
+                                                className="form-control"
+                                                /*value={product.manufacturer}
+                                                onChange={handleChange}*/
+                                            />
+                                        </div>
+                                        { /*
+                                        <div className="mb-3">
+                                            <label htmlFor="properties" className='form-label'>Tulajdonságok</label>
+                                            <textarea
+                                                id="properties"
+                                                name="properties"
+                                                className="form-control"
+                                                value={JSON.stringify(product.properties)}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                        */
+                                        }
+                                        <label className="form-label">Tulajdonságok:</label>
+                                            <div className="row my-4 my-sm-2" /*key={index}*/>
+                                                <div className="col-10 col-sm-11">
+                                                    <div className="row">
+                                                        <div className="col-12 col-sm-6 mb-2 mb-sm-0">
+                                                            <input
+                                                                type="text"
+                                                                className="form-control"
+                                                                /*value={property[0]}
+                                                                onChange={(e) => handleChange(e, index, undefined, true)}*/
+                                                                placeholder="Tulajdonság neve"
+                                                            />
+                                                        </div>
+                                                        <div className="col-12 col-sm-6">
+                                                            <input
+                                                                type="text"
+                                                                className="form-control"
+                                                                /*value={property[1]}
+                                                                onChange={(e) => handleChange(e, index, undefined, false)}*/
+                                                                placeholder="Érték"
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="col-1">
+                                                    <div className="d-flex h-100 align-items-center justify-content-center">
+                                                        <button
+                                                            className="btn btn-danger btn-sm"
+                                                            type='button'
+                                                            /*onClick={() => {
+                                                                const properties = Object.entries(newProduct.properties)
+                                                                const filteredProperties = properties.filter((_, i) => i !== index)
+                                                                const updatedProperties = filteredProperties.reduce((acc, [key, value]) => {
+                                                                    acc[key] = value
+                                                                    return acc
+                                                                }, {} as Record<string, string>)
+
+                                                                setProduct(prevState => prevState ? ({
+                                                                    ...prevState,
+                                                                    properties: updatedProperties
+                                                                }) : null)
+                                                            }}*/
+                                                        >
+                                                            <FontAwesomeIcon icon={faTrash as IconProp} />
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        <button
+                                            className="addImgButton"
+                                            type='button'
+                                            /*onClick={() => {
+                                                setProduct(prevState => {
+                                                    if (!prevState) return null
+                                                    return {
+                                                        ...prevState,
+                                                        properties: {
+                                                            ...prevState.properties,
+                                                            "": ""
+                                                        }
+                                                    }
+                                                })
+                                            }}*/
+                                        >
+                                            Új tulajdonság hozzáadása
+                                            <span><FontAwesomeIcon icon={faPlus as IconProp} /></span>
+                                        </button>
+                                        <div className="mb-3">
+                                            <label htmlFor="categoryId" className="form-label">Termékkategória:</label>
+                                            <select
+                                                id="categoryId"
+                                                name="categoryId"
+                                                className="form-control"
+                                                /*value={product.categoryId}
+                                                onChange={handleChange}*/
+                                            >
+                                                {Object.entries(categoryOptions).map(([id, name]) => (
+                                                    <option key={id} value={id}>{name}</option>
+                                                ))}
+                                            </select>
+                                        </div>
+                                        <button type="submit" className="orangeButton visually-hidden" id='productSumbit' name='productSumbit'>Mentés</button>
+                                    </form>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="blueButton" data-bs-dismiss="modal">Mégse</button>
+                            <button type="button" className="orangeButton" data-bs-dismiss="modal" /*onClick={handleProfilePicSubmit}*/>Mentés</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
